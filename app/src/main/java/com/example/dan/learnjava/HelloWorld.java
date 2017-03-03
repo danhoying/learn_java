449a467f2ee747f1dec149b9daa26a1fae61e815
Dan Hoying <danhoying@gmail.com>
2017-03-03 08:56:44 -0800

    Delete main method body

--- app/src/main/java/com/example/dan/learnjava/HelloWorld.java
+++ app/src/main/java/com/example/dan/learnjava/HelloWorld.java
@@ -6,26 +6,6 @@ package com.example.dan.learnjava;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("My first Java program");
 
-        String dan = "Dan Hoying";
-        System.out.println(dan);
-
-        int danWeeklySalary = 32;
-        int danMonthlySalary = danWeeklySalary * 4;
-        System.out.println("Weekly salary: " + danWeeklySalary);
-        System.out.println("Monthly salary: " + danMonthlySalary);
-
-        int apples = 6;
-        int oranges = 5;
-        int fruit = apples - oranges;
-        System.out.println("I have " + fruit + " fruit");
-
-        int weeks = 130;
-        double years = weeks / 52.0;
-        System.out.println(weeks + " weeks is " + years + " years");
-
-        System.out.println("Hello, " + dan + " here");
     }
 }
