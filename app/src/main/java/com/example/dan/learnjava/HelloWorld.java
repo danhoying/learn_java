5f1d4a377352df50255ef9e5dc3f35e3aad380a2
Dan Hoying <danhoying@gmail.com>
2017-03-03 08:26:56 -0800

    Add more strings and ints

--- app/src/main/java/com/example/dan/learnjava/HelloWorld.java
+++ app/src/main/java/com/example/dan/learnjava/HelloWorld.java
@@ -8,5 +8,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("My first Java program");
+
+        String dan = "Dan Hoying";
+        System.out.println(dan);
+
+        int danSalary = 32;
+        int monthly = danSalary * 4;
+        System.out.println(danSalary);
+        System.out.println(monthly);
     }
 }
