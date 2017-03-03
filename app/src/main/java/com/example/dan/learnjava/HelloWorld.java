309a667937d9e0ff05b3c043b610b2bd824c4cc7
Dan Hoying <danhoying@gmail.com>
2017-03-03 09:05:54 -0800

    Add boolean and conditional

--- app/src/main/java/com/example/dan/learnjava/HelloWorld.java
+++ app/src/main/java/com/example/dan/learnjava/HelloWorld.java
@@ -6,6 +6,14 @@ package com.example.dan.learnjava;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        int lives = 3;
+        boolean isGameOver = (lives < 1);
+        System.out.println(isGameOver);
 
+        if (isGameOver) {
+            System.out.println("Game over!");
+        } else {
+            System.out.println("You're still alive!");
+        }
     }
 }
