27a537320888c90bb7fab5144df30d89876733cd
Dan Hoying <danhoying@gmail.com>
2017-03-09 08:08:12 -0800

    Add printouts of levels and lives for all Players

--- app/src/main/java/com/example/dan/learnjava/Demo.java
+++ app/src/main/java/com/example/dan/learnjava/Demo.java
@@ -10,11 +10,18 @@ public class Demo {
         System.out.println(dan.getHandleName());
         dan.setHandleName("Dan");
         System.out.println(dan.getHandleName());
+        System.out.println("Level: " + dan.getLevel());
+        System.out.println("Lives: " + dan.getLives());
 
         Player louise = new Player("Louise");
         System.out.println(louise.getHandleName());
+        louise.setLives(5);
+        System.out.println("Level: " + louise.getLevel());
+        System.out.println("Lives: " + louise.getLives());
 
         Player gr8 = new Player("gr8", 9);
         System.out.println(gr8.getHandleName());
+        System.out.println("Level: " + gr8.getLevel());
+        System.out.println("Lives: " + gr8.getLives());
     }
 }
