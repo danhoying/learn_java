a3a27940b4d8048adacfeaa7922319cc0747c5a8
Dan Hoying <danhoying@gmail.com>
2017-03-14 08:29:45 -0700

    Remove extra Players and add Weapon printout

--- app/src/main/java/com/example/dan/learnjava/Demo.java
+++ app/src/main/java/com/example/dan/learnjava/Demo.java
@@ -6,11 +6,11 @@ package com.example.dan.learnjava;
 
 public class Demo {
     public static void main(String[] args) {
-        Player dan = new Player();
+        Player dan = new Player("Tom Smith, 8");
         System.out.println(dan.getHandleName());
-        dan.setNameAndLevel("Daniel", 5);
-        System.out.println(dan.getHandleName());
+
         System.out.println("Level: " + dan.getLevel());
         System.out.println("Lives: " + dan.getLives());
+        System.out.println(dan.getWeapon().getName());
     }
 }
