1d599dff77684398a1345f7f7fa117f1e6833684
Dan Hoying <danhoying@gmail.com>
2017-03-15 07:59:44 -0700

    Add new Weapon object and create Loot class

--- app/src/main/java/com/example/dan/learnjava/Demo.java
+++ app/src/main/java/com/example/dan/learnjava/Demo.java
@@ -8,9 +8,12 @@ public class Demo {
     public static void main(String[] args) {
         Player dan = new Player("Tom Smith, 8");
         System.out.println(dan.getHandleName());
-
         System.out.println("Level: " + dan.getLevel());
         System.out.println("Lives: " + dan.getLives());
+        System.out.println(dan.getWeapon().getName());
+
+        Weapon myAxe = new Weapon("Goldshine Axe", 15, 50);
+        dan.setWeapon(myAxe);
         System.out.println(dan.getWeapon().getName());
     }
 }
---
+++ app/src/main/java/com/example/dan/learnjava/Loot.java
@@ -0,0 +1,9 @@
+package com.example.dan.learnjava;
+
+/**
+ * Created by dan on 3/15/17.
+ */
+
+public class Loot {
+
+}
