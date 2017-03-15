bee8ca76841511ae36cf5e488516d82acc65bd7a
Dan Hoying <danhoying@gmail.com>
2017-03-15 08:03:26 -0700

    Create constructors, getters, setters for Loot class

--- app/src/main/java/com/example/dan/learnjava/Loot.java
+++ app/src/main/java/com/example/dan/learnjava/Loot.java
@@ -4,6 +4,40 @@ package com.example.dan.learnjava;
  * Created by dan on 3/15/17.
  */
 
-public class Loot {
+enum LootType { POTION, RING, ARMOR }
 
+public class Loot {
+    private String name;
+    private LootType type;
+    private int value;
+
+    public Loot(String name, LootType type, int value) {
+        this.name = name;
+        this.type = type;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LootType getType() {
+        return type;
+    }
+
+    public void setType(LootType type) {
+        this.type = type;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
