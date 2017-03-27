893ff7f6b775e89224b1dcd1595adb94cfdad629
Dan Hoying <danhoying@gmail.com>
2017-03-27 13:20:21 -0700

    Add additional Loot pickups

--- app/src/main/java/com/example/dan/learnjava/Demo.java
+++ app/src/main/java/com/example/dan/learnjava/Demo.java
@@ -9,17 +9,21 @@ import java.util.ArrayList;
 public class Demo {
     public static void main(String[] args) {
         Player dan = new Player("Tom Smith, 8");
-        System.out.println(dan.getHandleName());
-        System.out.println("Level: " + dan.getLevel());
-        System.out.println("Lives: " + dan.getLives());
-        System.out.println(dan.getWeapon().getName());
+//        System.out.println(dan.getHandleName());
+//        System.out.println("Level: " + dan.getLevel());
+//        System.out.println("Lives: " + dan.getLives());
+//        System.out.println(dan.getWeapon().getName());
 
         Weapon myAxe = new Weapon("Goldshine Axe", 15, 50);
         dan.setWeapon(myAxe);
-        System.out.println(dan.getWeapon().getName());
+//        System.out.println(dan.getWeapon().getName());
 
         Loot redPotion = new Loot("Red Potion", LootType.POTION, 7);
         dan.pickUpLoot(redPotion);
+
+        dan.pickUpLoot(new Loot("+3 Chest Armor", LootType.ARMOR, 80));
+        dan.pickUpLoot(new Loot("Ring of Protection +2", LootType.RING, 40));
+        dan.pickUpLoot(new Loot("Invisibility Potion", LootType.POTION, 35));
 
         ArrayList<Loot> allItems = dan.getInventory();
 
