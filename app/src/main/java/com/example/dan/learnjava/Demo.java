c901556763b68a107ace8f2e2be01007b3067c18
Dan Hoying <danhoying@gmail.com>
2017-03-29 07:34:33 -0700

    Delete everything in Demo main

--- app/src/main/java/com/example/dan/learnjava/Demo.java
+++ app/src/main/java/com/example/dan/learnjava/Demo.java
@@ -6,28 +6,6 @@ package com.example.dan.learnjava;
 
 public class Demo {
     public static void main(String[] args) {
-        Player dan = new Player("Tom Smith, 8");
-//        System.out.println(dan.getHandleName());
-//        System.out.println("Level: " + dan.getLevel());
-//        System.out.println("Lives: " + dan.getLives());
-//        System.out.println(dan.getWeapon().getName());
 
-        Weapon myAxe = new Weapon("Goldshine Axe", 15, 50);
-        dan.setWeapon(myAxe);
-//        System.out.println(dan.getWeapon().getName());
-
-        Loot redPotion = new Loot("Red Potion", LootType.POTION, 7);
-        dan.pickUpLoot(redPotion);
-
-        dan.pickUpLoot(new Loot("+3 Chest Armor", LootType.ARMOR, 80));
-        dan.pickUpLoot(new Loot("Ring of Protection +2", LootType.RING, 40));
-        dan.pickUpLoot(new Loot("Invisibility Potion", LootType.POTION, 35));
-
-        dan.showInventory();
-
-//        Loot bluePotion = new Loot("Blue Potion", LootType.POTION, 6);
-        boolean wasDeleted = dan.dropLoot(redPotion);
-        System.out.println(wasDeleted);
-        dan.showInventory();
     }
 }
