23605073557da7c140e0e9cf7ee5f879bfea2249
Dan Hoying <danhoying@gmail.com>
2017-03-29 07:36:37 -0700

    Add Enemy class and initialize

---
+++ app/src/main/java/com/example/dan/learnjava/Enemy.java
@@ -0,0 +1,41 @@
+package com.example.dan.learnjava;
+
+/**
+ * Created by dan on 3/29/17.
+ */
+
+public class Enemy {
+    private String name;
+    private int hitPoints;
+    private int lives;
+
+    public Enemy(String name, int hitPoints, int lives) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.lives = lives;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
+}
