7f4f90239ee172953eadaff07278240bf3dd7476
Dan Hoying <danhoying@gmail.com>
2017-03-29 07:51:29 -0700

    Add printout to test Enemy in Demo

--- app/src/main/java/com/example/dan/learnjava/Demo.java
+++ app/src/main/java/com/example/dan/learnjava/Demo.java
@@ -6,6 +6,11 @@ package com.example.dan.learnjava;
 
 public class Demo {
     public static void main(String[] args) {
-
+        Enemy enemy = new Enemy("Slime", 40, 3);
+        enemy.showInfo();
+        enemy.takeDamage(3);
+        enemy.showInfo();
+        enemy.takeDamage(38);
+        enemy.showInfo();
     }
 }
