cd1aadcabfe7adf65ccef378bf93d934462606f0
Dan Hoying <danhoying@gmail.com>
2017-04-04 08:15:03 -0700

    Create Vampyre Enemy

---
+++ app/src/main/java/com/example/dan/learnjava/Vampyre.java
@@ -0,0 +1,11 @@
+package com.example.dan.learnjava;
+
+/**
+ * Created by dan on 4/4/17.
+ */
+
+public class Vampyre extends Enemy {
+    public Vampyre(String name) {
+        super(name, 20, 3);
+    }
+}
